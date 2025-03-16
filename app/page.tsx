a408e93fc27bfba21a2fd735806e1e83fe76debd
boklino <youssefalbokl@gmail.com>
2025-03-17 00:27:39 +0200

    test

--- app/page.tsx
+++ app/page.tsx
@@ -182,39 +182,7 @@ export default function Home() {
                     <span className="text-teal-500">03.</span> Contact
                   </button>
                 </li>
-                <li>
-                  <button
-                    onClick={toggleDarkMode}
-                    className="p-1 rounded-full transition-transform hover:scale-125"
-                    aria-label={
-                      darkMode ? "Switch to light mode" : "Switch to dark mode"
-                    }
-                  >
-                    {darkMode ? (
-                      <svg
-                        className="w-6 h-6 text-white"
-                        xmlns="http://www.w3.org/2000/svg"
-                        viewBox="0 0 24 24"
-                        fill="currentColor"
-                      >
-                        <path d="M12 2.25a.75.75 0 01.75.75v2.25a.75.75 0 01-1.5 0V3a.75.75 0 01.75-.75zM7.5 12a4.5 4.5 0 119 0 4.5 4.5 0 01-9 0zM18.894 6.166a.75.75 0 00-1.06-1.06l-1.591 1.59a.75.75 0 101.06 1.061l1.591-1.59zM21.75 12a.75.75 0 01-.75.75h-2.25a.75.75 0 010-1.5H21a.75.75 0 01.75.75zM17.834 18.894a.75.75 0 001.06-1.06l-1.59-1.591a.75.75 0 10-1.061 1.06l1.59 1.591zM12 18a.75.75 0 01.75.75V21a.75.75 0 01-1.5 0v-2.25A.75.75 0 0112 18zM7.758 17.303a.75.75 0 00-1.061-1.06l-1.591 1.59a.75.75 0 001.06 1.061l1.591-1.59zM6 12a.75.75 0 01-.75.75H3a.75.75 0 010-1.5h2.25A.75.75 0 016 12zM6.697 7.757a.75.75 0 001.06-1.06l-1.59-1.591a.75.75 0 00-1.061 1.06l1.59 1.591z" />
-                      </svg>
-                    ) : (
-                      <svg
-                        className="w-6 h-6 text-gray-800"
-                        xmlns="http://www.w3.org/2000/svg"
-                        viewBox="0 0 24 24"
-                        fill="currentColor"
-                      >
-                        <path
-                          fillRule="evenodd"
-                          d="M9.528 1.718a.75.75 0 01.162.819A8.97 8.97 0 009 6a9 9 0 009 9 8.97 8.97 0 003.463-.69.75.75 0 01.981.98 10.503 10.503 0 01-9.694 6.46c-5.799 0-10.5-4.701-10.5-10.5 0-4.368 2.667-8.112 6.46-9.694a.75.75 0 01.818.162z"
-                          clipRule="evenodd"
-                        />
-                      </svg>
-                    )}
-                  </button>
-                </li>
+
                 <li>
                   <Link
                     href="https://drive.google.com/file/d/1iCicrDEO4L7GAMGIw2kl_cQGznwxh-K7/view?usp=sharing"
@@ -474,7 +442,7 @@ export default function Home() {
                     className="block overflow-hidden rounded-lg shadow-lg transition-transform hover:opacity-90"
                   >
                     <Image
-                      src="/images/boktel.png"
+                      src="/images/Boktel.png"
                       alt="Boktel project screenshot"
                       width={700}
                       height={400}
@@ -621,7 +589,7 @@ export default function Home() {
                     className="block overflow-hidden rounded-lg shadow-lg transition-transform hover:opacity-90"
                   >
                     <Image
-                      src="/images/countries.png"
+                      src="/images/countries.PNG"
                       alt="Countries Information project screenshot"
                       width={700}
                       height={400}
